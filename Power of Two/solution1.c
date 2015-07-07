0842fda28d8524b46cfedf372c9f3c36f2af7860
Sunny <ratsunny@gmail.com>
2015-07-07 11:25:13 +0800

    Add a C solution for Power of Two

---
+++ Power of Two/solution1.c
@@ -0,0 +1,9 @@
+bool isPowerOfTwo(int n) {
+    if (n < 0) { return false; }
+    n = n - ((n >> 1) & 0x55555555);
+    n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+    n = (n + (n >> 4)) & 0x0f0f0f0f;
+    n = n + (n >> 8);
+    n = n + (n >> 16);
+    return (n & 0x3f) == 1;
+}
