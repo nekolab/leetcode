31913354e2fc12bec0a481c05d2c6b59771b2483
Sunny <ratsunny@gmail.com>
2015-07-12 18:52:09 +0800

    Add a C solution for Lowest Common Ancestor of a Binary Search Tree

---
+++ Lowest Common Ancestor of a Binary Search Tree/solution1.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if (root == NULL) { return; }
+
+    if (root->val > p->val && root->val > q->val) {
+        return lowestCommonAncestor(root->left, p, q);
+    }
+
+    if (root->val < p->val && root->val < q->val) {
+        return lowestCommonAncestor(root->right, p, q);
+    }
+
+    return root;
+}
