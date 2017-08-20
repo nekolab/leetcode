d22e8069d9160662151e1e1cf86d20cd166cd9e3
Sunny <ratsunny@gmail.com>
2017-08-21 01:57:05 +0800

    Two Sum: Update code to fit the latest problem

--- Two Sum/solution1.js
+++ Two Sum/solution1.js
@@ -7,12 +7,12 @@ var twoSum = function(nums, target) {
 
     var i, index, map = {};
     for (i = 0; i < nums.length; ++i) {
-        map[target - nums[i]] = { index: i + 1 };
+        map[target - nums[i]] = { index: i };
     }
 
     for (i = 0; i < nums.length; ++i) {
-        if (nums[i] in map && ((index = map[nums[i]].index) != (i + 1))) {
-            return (index > i + 1) ? [i + 1, index] : [index, i + 1];
+        if (nums[i] in map && ((index = map[nums[i]].index) != i)) {
+            return (index > i) ? [i, index] : [index, i];
         }
     }
 };
